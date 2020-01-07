6dd4f2beb1ec5306ea6de6f8cdf54ca85ba596b9
mdm5183 <52705262+mdm5183@users.noreply.github.com>
2020-01-07 11:22:48 -0500

    Add files via upload

--- website/app.js
+++ website/app.js
@@ -12,73 +12,62 @@ const url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${a
 document.getElementById('generate').addEventListener('click', performAction);
 
 /* Function called by event listener */
-function performAction(e){
-	const newZipCode = document.getElementById('zip').value;
-	const newContent = document.getElementById('feelings').value;
-
-	getWeather(baseURL, newZipCode, apiKey)
-	  	.then(function(data){
-	    console.log(data);
-	    postData('/add', {date:newDate, temp:data.main.temp, content:newContent} );
-	  })
-	  .then(
-	    updateUI()
-	  )
-	}
+function performAction(e) {
+	const newZip = document.getElementById('zip').value;
+	const newResponse = document.getElementById('feelings').value;
+	
+	getWeather(baseURL, newZip, apiKey)
+	.then(function(data) {
+		console.log(data)
+		console.log(newResponse)
+		postData('/add', {date:data.date, temperature:data.main.temp, content:newResponse})
+	})
+	.then(
+		updateUI()
+		)
+	};
 
 /* Function to GET Web API Data*/
-const getWeather = async (baseURL, newZipCode, apiKey) =>{ 
-  const request = await fetch(baseURL + newZipCode + apiKey);
+const getWeather = async (baseURL, newZip, apiKey)=>{
+  const res = await fetch(baseURL+newZip+apiKey)
   try {
-  const data = await request.json();
-  return data;
-  }
-  catch(error) {
+    const data = await res.json();
+    return data;
+  }catch(error) {
     console.log("error", error);
   }
-};
-
-/* Function to POST data */
-const postData = async ( url = '', data = {})=>{
-    const response = await fetch(url, {
-    method: 'POST', 
-    mode: 'cors',
-    credentials: 'same-origin', 
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-    	date: data.date,
-    	temp: data.main.temp,
-    	content: data.content,
-    })      
-  });
-
-    try {
-      const newData = await request.json();
-      return newData;
-    }catch(error) {
-    console.log("error", error);
-    }
-};
-
-//update UI
-const updateUI = async () => {
-	const request = await fetch('/all');
-	const projectData = await request.json();
-	document.getElementById('date').innerHTML = `${projectData.date}`;
-	document.getElementById('temp').innerHTML = `${projectData.temp}`;
-	document.getElementById('content').innerHTML = projectData.content;
 }
 
-// const updateUI = async () => {
-//   const request = await fetch('/all');
-//   try{
-//     const projectData = await request.json();
-//     document.getElementById('date').innerHTML = allData.date;
-//     document.getElementById('temp').innerHTML = allData.temp;
-//     document.getElementById('content').innerHTML = allData.content;
-// }catch(error){
-//     console.log("error", error);
-//   }
-// }
+/* Function to POST data */
+const postData = async ( url = 'http://localhost:5000/add', data = {})=> {
+	const response = await fetch(url, {
+		method: 'POST',
+		mode: 'cors',
+		credentials: 'same-origin',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),	
+		});
+	try {
+		const newData = await response.json();
+		console.log(newData);
+		return(newData)
+	}catch(error) {
+		console.log("error", error);
+	}
+}
+
+/* Function to GET Project Data */
+const updateUI = async () => {
+	const request = await fetch('/all');
+	try{
+		const allData = await request.json();
+		console.log(allData);
+		document.getElementById('date').innerHTML = allData[0].date;
+		document.getElementById('temp').innerHTML = allData[0].temperature;
+		document.getElementById('content').innerHTML = allData[0].newResponse;
+	}catch(error) {
+		console.log("error", error);
+	}
+}
