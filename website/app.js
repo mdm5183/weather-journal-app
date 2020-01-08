ee34bb48087bd937c962442ba8b59c6da293c33b
mdm5183 <52705262+mdm5183@users.noreply.github.com>
2020-01-08 10:51:07 -0500

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,6 +1,21 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+var n = month[d.getMonth()];
+let newDate = n+' '+ d.getDate()+', '+ d.getFullYear();
+
 console.log(newDate);
 
 // Personal API Key for OpenWeatherMap API
